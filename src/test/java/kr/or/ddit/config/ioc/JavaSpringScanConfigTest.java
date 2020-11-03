28bb2dcbf389b3307eaa02026ed5c7b9da875f40
whc <tofan12312@gmail.com>
2020-11-03 09:17:23 +0900

    스프링 설정 java config java config componentScan

--- src/test/java/kr/or/ddit/config/ioc/JavaSpringScanConfigTest.java
+++ src/test/java/kr/or/ddit/config/ioc/JavaSpringScanConfigTest.java
@@ -6,7 +6,6 @@ import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -16,8 +15,8 @@ import kr.or.ddit.board.service.BoardServiceI;
 import kr.or.ddit.ioc.JavaSpringConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {JavaSpringConfig.class})
-public class JavaSpringConfigTest {
+@ContextConfiguration(classes = {JavaSpringScanConfig.class})
+public class JavaSpringScanConfigTest {
 
 	@Resource(name="boardRepository")
 	private BoardRepositoryI boardRepository;
@@ -40,5 +39,7 @@ public class JavaSpringConfigTest {
 	}
 	
 	// ComponentScanTest에서 바뀐 부분 : ~.xml 을 읽은건지, 아니면 이를 대체하는 java 파일을 읽은 건지 !
-
+	// 왜 xml 보다 java를 권장하는가 ? Eclipse와 같은 툴을 사용하지 않는 이상, xml(그냥 txt 파일이다.)의 오류를 발견하기는 어렵다.
+	// 따라서 xml은 테스트 코드를 돌리거나 서버에서 돌릴 때 에러를 알 수 있다.
+	// 이에 비해 java 파일은 코드로 작성되기 때문에 compile 시점에서 알 수 있다.
 }
