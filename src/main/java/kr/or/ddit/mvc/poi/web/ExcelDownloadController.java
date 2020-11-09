2a29c9cd94d0b938f6a430b1de7c0c858c156673
whc <tofan12312@gmail.com>
2020-11-09 17:05:51 +0900

    실습  poi - exceldownloadView

---
+++ src/main/java/kr/or/ddit/mvc/poi/web/ExcelDownloadController.java
@@ -0,0 +1,41 @@
+package kr.or.ddit.mvc.poi.web;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.or.ddit.member.model.MemberVo;
+
+@Controller
+public class ExcelDownloadController {
+	@RequestMapping("/excelDown")
+	public String excelDown(Model model) {
+		List<String> header = new ArrayList<String>();
+		header.add("userid");
+		header.add("usernm");
+		
+		// Model 객체에 header, data
+		List<Map<String, String>> data = new ArrayList<>();
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("userid", "brown");
+		map.put("usernm", "브라운");
+		
+		Map<String, String> map2 = new HashMap<String, String>();
+		map2.put("userid", "sally");
+		map2.put("usernm", "샐리");
+		
+		data.add(map);
+		data.add(map2);
+		
+		model.addAttribute("header", header);
+		model.addAttribute("data", data);
+		
+		return "excelView";
+	}
+}
